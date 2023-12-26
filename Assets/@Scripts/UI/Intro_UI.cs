21937af73ffe6a65e1008cd34fed5955912f6e72
정민 <jeongmin322@naver.com>
2023-12-26 19:39:42 +0900

    Feat : Apply Addressable to IntroScene
    IntroScene에 Addressable 적용

---
+++ Assets/@Scripts/UI/Intro_UI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Intro_UI : MonoBehaviour
+{
+    #region Init
+
+    private void Start()
+    {
+        LoadAssetsWithLabels();
+    }
+
+    #endregion
+
+    #region Methods
+
+    private void LoadAssetsWithLabels()
+    {
+        string[] labels = { "Preload", "IntroScene" };
+
+        foreach (var label in labels)
+        {
+            Addressables.LoadResourceLocationsAsync(label).Completed +=
+                (handle) =>
+                {
+                    var locations = handle.Result;
+
+                    foreach (var location in locations)
+                    {
+                        Addressables.InstantiateAsync(location);
+                    }
+                };
+        }
+    }
+
+    #endregion
+}
