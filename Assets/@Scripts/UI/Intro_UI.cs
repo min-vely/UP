dfaeb5fd0d446bafe5b1493829ef720990162412
정민 <jeongmin322@naver.com>
2023-12-27 11:42:31 +0900

    Fix : Solve NullReferenceException
    IntroScene 로드 시 NullReferenceException 뜨는 현상 수정

--- Assets/@Scripts/UI/Intro_UI.cs
+++ Assets/@Scripts/UI/Intro_UI.cs
@@ -1,5 +1,7 @@
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
 
 public class Intro_UI : MonoBehaviour
 {
@@ -16,7 +18,7 @@ public class Intro_UI : MonoBehaviour
 
     private void LoadAssetsWithLabels()
     {
-        string[] labels = { "Preload", "IntroScene" };
+        string[] labels = { "IntroScene", "Preload" };
 
         foreach (var label in labels)
         {
@@ -28,8 +30,20 @@ public class Intro_UI : MonoBehaviour
 
                     foreach (var location in locations)
                     {
-                        // 받아온 리소스 위치를 이용해 각 리소스를 인스턴스화
-                        Addressables.InstantiateAsync(location);
+                        if (location == null)
+                        {
+                            Debug.Log("리소스 위치가 null임");
+                        }
+                        else
+                        {
+                            Debug.Log($"로케이션: {location}");
+
+                            // 리소스 타입이 프리팹인 경우에만 인스턴스화
+                            if (location.ResourceType == typeof(GameObject))
+                            {
+                                Addressables.InstantiateAsync(location);
+                            }
+                        }
                     }
                 };
         }
